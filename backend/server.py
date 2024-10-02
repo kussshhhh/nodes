7463d30122cc11e839e86ccafea71a42ee35b44f
kussshhhh <kushsep21@gmail.com>
2024-10-02 23:29:55 +0530

    sidebar goes brrr

--- backend/server.py
+++ backend/server.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Configure the Gemini API
-genai.configure(api_key='AIzaSyDzkvzoICjKXhsMs0pScbEDtlOLpaoJDOw')
+genai.configure(api_key='')
 
 def create_connection():
     conn = None
