3e883b07d9312c22f53363a6dae7c3b560c35216
kussshhhh <kushsep21@gmail.com>
2024-10-05 20:23:13 +0530

    it should work now

--- frontend/src/components/Graph.js
+++ frontend/src/components/Graph.js
@@ -11,7 +11,7 @@ import CustomNode from './CustomNode';
 import Popup from './Popup';
 import { generateNodes, generateEdges } from './graphUtils';
 import { updateNodeContent } from './IndexDBUtils';
-const backendapi = "http://127.0.0.1:5000" || process.env.REACT_APP_API_URL  ;
+const backendapi = process.env.REACT_APP_API_URL  ;
 const nodeTypes = {
   custom: CustomNode,
 };
