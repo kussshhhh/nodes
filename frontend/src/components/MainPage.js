aa0d81798fb68ff49ddec2fe990367e76662ddcc
dumball <archit1290@gmail.com>
2024-10-06 01:06:50 +0530

    a

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -10,7 +10,7 @@
   // import { is } from '@react-three/fiber/dist/declarations/src/core/utils'
   
 
-  const backendapi =  process.env.REACT_APP_API_URL ;
+  const backendapi = "http://localhost:5000" ;
   console.log(backendapi) 
 
   function createMoonTexture() {
